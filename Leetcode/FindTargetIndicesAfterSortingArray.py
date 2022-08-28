a51493a62e38209716cb9296a2972ee19025b3e4
Animesh Sharma <animesharma3@gmail.com>
2022-08-28 18:02:04 +0530

    Find Target Indices 1

--- Leetcode/FindTargetIndicesAfterSortingArray.py
+++ Leetcode/FindTargetIndicesAfterSortingArray.py
@@ -6,3 +6,15 @@ class Solution:
             if nums[i] == target:
                 res.append(i)
         return res
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        smaller, larger = 0, 0
+        for num in nums:
+            if num < target:
+                smaller+=1
+            elif num > target:
+                larger+=1
+        res = []
+        for i in range(smaller, len(nums)-larger):
+            res.append(i)            
+        return res
+            
