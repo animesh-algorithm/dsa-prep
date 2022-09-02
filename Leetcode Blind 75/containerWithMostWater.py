7990f9e6f6da2baf6ce7d693c18979b877880fdf
Animesh Sharma <animesharma3@gmail.com>
2022-09-02 09:24:52 +0530

    Leetcode Blind 75 - Container with most water - Two Pointer Approach 2

--- Leetcode Blind 75/containerWithMostWater.py
+++ Leetcode Blind 75/containerWithMostWater.py
@@ -11,3 +11,19 @@ class Solution:
                 j+=1
             i+=1
         return maxA
+    
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i=0
+        j=i+1
+        maxA = float("-inf")
+        while i<len(height)-1:
+            if j==len(height):
+                i+=1
+                j=i+1
+                continue
+            currA = min(height[i], height[j])*(j-i)
+            if maxA < currA:
+                maxA = currA
+            j+=1
+        return maxA
