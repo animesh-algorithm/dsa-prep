a930596e6def9cf275d4a2e03b7e8a73814fcd58
Animesh Sharma <animesharma3@gmail.com>
2022-09-03 16:18:36 +0530

    Max depth using BFS

--- Leetcode/MaxDepthOfBinaryTree.py
+++ Leetcode/MaxDepthOfBinaryTree.py
@@ -13,3 +13,30 @@ class Solution:
                         fun(root.right, depth+1))
             return depth
         return fun(root, 0)
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def levelOrder(root: TreeNode) -> List[List[int]]:
+            result = []
+
+            if root == None:
+                return result
+
+            Queue = []
+            Queue.append(root)
+
+            while len(Queue):
+                size = len(Queue)
+                currentLevel = []
+                for i in range(size):
+                    current = Queue.pop()
+                    currentLevel.append(current.val)
+                    if current.left != None:
+                        Queue.insert(0, current.left)
+                    if current.right != None:
+                        Queue.insert(0, current.right)
+                if currentLevel != []:
+                    result.append(currentLevel)
+            return result
+        return len(levelOrder(root))
