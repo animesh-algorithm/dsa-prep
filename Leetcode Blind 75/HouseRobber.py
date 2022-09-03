06374db4088e7573478b0f93426f29391924f0e1
Animesh Sharma <animesharma3@gmail.com>
2022-09-03 17:05:20 +0530

    House Robber - Memoization

--- Leetcode Blind 75/HouseRobber.py
+++ Leetcode Blind 75/HouseRobber.py
@@ -7,3 +7,22 @@ class Solution:
                 return 0
             return max(nums[i]+fun(i-2), 0+fun(i-1))
         return fun(len(nums)-1)
+
+
+class Solution:
+
+
+def rob(self, nums: List[int]) -> int:
+    def fun(i, dp):
+        if i == 0:
+            dp[i] = nums[i]
+            return dp[i]
+        if dp[i] != -1:
+            return dp[i]
+        if i < 1:
+            dp[i] = 0
+            return dp[i]
+        dp[i] = max(nums[i]+fun(i-2, dp), 0+fun(i-1, dp))
+        return dp[i]
+    dp = [-1]*(len(nums))
+    return fun(len(nums)-1, dp)
