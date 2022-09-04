b055474fee2327ad8e64b336b1ae74623e121c76
Animesh Sharma <animesharma3@gmail.com>
2022-09-04 20:58:53 +0530

    Duplicate Zeros

--- Leetcode/DuplicateZeros.py
+++ Leetcode/DuplicateZeros.py
@@ -3,15 +3,14 @@ class Solution:
         """
         Do not return anything, modify arr in-place instead.
         """
-        i   = 0
-        n   = len(arr)
+        i = 0
+        n = len(arr)
         while i < len(arr):
-            if arr[i]  = = 0:
+            if arr[i] == 0:
                 arr.insert(i+1, 0)
-                i   = i+2
+                i = i+2
                 continue
-            i  + = 1
+            i + = 1
         n1 = len(arr)
         for i in range(n1-n):
             arr.pop()
-
