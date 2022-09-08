12c717b6888f8a2978fa5151d4777616a53b988a
Animesh Sharma <animesharma3@gmail.com>
2022-09-08 12:03:48 +0530

    Find First and Last Position

--- Leetcode - Binary Search - Study Plan/Day 5/findFirstAndLastPosition.py
+++ Leetcode - Binary Search - Study Plan/Day 5/findFirstAndLastPosition.py
@@ -13,3 +13,26 @@ class Solution:
         if i >= len(a):
             i = -1
         return [i, j]
+
+    class Solution:
+
+
+def searchRange(self, a: List[int], target: int) -> List[int]:
+    def solve(s, e, first=False, ans=-1):
+        if s <= e:
+            mid = s+(e-s)//2
+            if a[mid] > target:
+                return solve(s, mid-1, first, ans)
+            elif a[mid] < target:
+                return solve(mid+1, e, first, ans)
+            else:
+                ans = mid
+                if first:
+                    return solve(s, mid-1, first, ans)
+                else:
+                    return solve(mid+1, e, first, ans)
+        return ans
+    res = [-1, -1]
+    res[0] = solve(0, len(a)-1, True, -1)
+    res[1] = solve(0, len(a)-1, False, -1)
+    return res
