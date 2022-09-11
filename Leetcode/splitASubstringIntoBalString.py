77afcc684b854d13ef0ccffdfa31a26a02eab23a
Animesh Sharma <animesharma3@gmail.com>
2022-09-12 04:58:43 +0530

    Split a String in Balanced Strings

--- Leetcode/splitASubstringIntoBalString.py
+++ Leetcode/splitASubstringIntoBalString.py
@@ -11,3 +11,16 @@ class Solution:
             if r == l:
                 bal += 1
         return bal
+
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        bal, res = 0, 0
+        for i in range(len(s)):
+            if s[i] == 'L':
+                bal += 1
+            elif s[i] == 'R':
+                bal -= 1
+            if bal == 0:
+                res += 1
+        return res
