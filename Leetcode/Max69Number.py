af13db2ea1fbf9d35081873159dde50b5fd58e46
Animesh Sharma <animesharma3@gmail.com>
2022-09-12 05:20:05 +0530

    Max 69 Number #2

--- Leetcode/Max69Number.py
+++ Leetcode/Max69Number.py
@@ -8,3 +8,17 @@ class Solution:
             res.append(int("".join(num)))
             num[i] = temp
         return max(res)
+
+
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        res = num
+        num = list(str(num))
+        for i in range(len(num)):
+            temp = num[i]
+            num[i] = '9' if num[i] == '6' else '6'
+            no = int("".join(num))
+            if res < no:
+                res = no
+            num[i] = temp
+        return res
